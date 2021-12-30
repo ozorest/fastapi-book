9004f641c84fecda3ca331328eb4390808125d7b
Thiago Ozores <tozores@gmail.com>
2021-12-29 21:12:08 -0300

    chapter4 working with pydantic objects

---
+++ chapter4/ch4_working_pydantic_objects.py
@@ -0,0 +1,54 @@
+from datetime import date
+from enum import Enum
+from typing import List
+
+from pydantic import BaseModel, ValidationError
+
+
+class Gender(str, Enum):
+    MALE = "MALE"
+    FEMALE = "FEMALE"
+    NON_BINARY = "NON_BINARY"
+
+
+class Address(BaseModel):
+    street_address: str
+    postal_code: str
+    city: str
+    country: str
+
+
+class Person(BaseModel):
+    first_name: str
+    last_name: str
+    gender: Gender
+    birthdate: date
+    interests: List[str]
+    address: Address
+
+
+try:
+    p = Person(
+        first_name="Jonh",
+        last_name="Doe",
+        # gender="INVALID_VALUE", # cause validation error
+        gender=Gender.MALE,
+        birthdate="1991-01-01",
+        # birthdate="1991-02-31", # cause validation error
+        interests=["travel", "sports"],
+        address={
+            "street_address": "Rua Pedro de Toledo 2302",
+            "postal_code": "13333320",
+            "city": "Indaiatuba",
+            "country": "Brazil"
+        }
+    )
+    person_dict = p.dict()
+    person_include = p.dict(include={"first_name", "last_name"})
+    person_exclude = p.dict(exclude={"birthdate", "interests"})
+    print(person_dict["first_name"])
+    print(person_dict["address"]["street_address"])
+    print(person_include)
+    print(person_exclude)
+except ValidationError as e:
+    print(str(e))
